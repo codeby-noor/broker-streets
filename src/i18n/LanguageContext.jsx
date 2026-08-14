619f6a6315c7384236f4b1704207148f5fb242d5
codeby-noor <mahenoorshaikh0408@gmail.com>
2026-08-14 12:42:37 +0530

    ii

--- src/i18n/LanguageContext.jsx
+++ src/i18n/LanguageContext.jsx
@@ -1,4 +1,4 @@
-fimport { createContext, useContext, useEffect, useMemo, useState } from 'react';
+import { createContext, useContext, useEffect, useMemo, useState } from 'react';
 import { defaultLanguage, getPropertyDisplayTitle, languageStorageKey, locationTranslationsGu, translations } from './translations';
 
 const LanguageContext = createContext(null);
